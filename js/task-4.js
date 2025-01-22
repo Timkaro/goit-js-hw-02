d77559cd8209593c3a66cc9bbed0f615b930ca06
Timkaro <timkaro@i.ua>
2025-01-22 13:55:33 +0100

    change task-4, task-1

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,24 @@
 // Доставка товару
 
 function getShippingCost(country) {
-  let price = 0;
-  let delivery = `Shipping to ${country} will cost ${price} credits`;
+  let price;
   switch (country) {
     case "China":
       price = 100;
-      return delivery;
       break;
     case "Chile":
       price = 250;
-      return delivery;
       break;
     case "Australia":
       price = 170;
-      return delivery;
       break;
     case "Jamaica":
       price = 120;
-      return delivery;
       break;
     default:
       return `Sorry, there is no delivery to your country`;
   }
+  return `Shipping to ${country} will cost ${price} credits`;
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
